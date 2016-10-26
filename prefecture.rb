65d236ea0f998d702fed7fc2ffc6f5d1f4dd0868
takeshi.watanabe <takeshi.watanabe@halenohi.jp>
2016-10-26 16:18:28 +0900

    modify method

--- prefecture.rb
+++ prefecture.rb
@@ -1,24 +1,16 @@
+require "json"
+
 class Area
   def initialize
-    require "json"
-
     json_file = File.open(__dir__ + "/prefecture.json")
     @json_data = JSON.parse(json_file.read)
   end
 
   def kantou
-    @count = 1
     @json_data.each do |data|
-      if @count >= 10
-        @count = "0" + @count.to_s
-      else
-        @count.to_s
+      if data[1]["area"]["id"] == 2
+        puts data[1]["name"]
       end
-
-      if data[@count]["area"]["id"] == 2
-        puts data[@count]["name"]
-      end
-      @count.to_i + 1
     end
   end
 end
